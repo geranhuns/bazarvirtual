397793891b81ab3911dce795705316546b9bb0ff
geranhuns <gera.nuncio@gmail.com>
2024-06-17 19:24:40 -0600

    fixed bg color

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -3,9 +3,9 @@ import RegisterForm from "@/components/RegisterForm/RegisterForm";
 export default function page() {
   return (
     <>
-      <main className="flex flex-col items-center h-screen justify-center bg-gray-100 ">
+      <main className="flex flex-col items-center h-screen justify-center  ">
         <h1 className="text-4xl text-black mb-6 font-bold">Crea tu cuenta</h1>
-        <h2 className="text-xl text-gray-500 mb-6">
+        <h2 className="text-xl text-gray-500 mb-6 w-96">
           Crea una cuenta para hacer tus compras y apoyar a los emprendedores
           locales
         </h2>
