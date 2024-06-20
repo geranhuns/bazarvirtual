6d3e6b7d6d10a20bdda38ec862c36cbb128c30a5
geranhuns <gera.nuncio@gmail.com>
2024-06-19 21:35:52 -0600

    changed logo navigation to home

--- src/components/Logos/LogoH.js
+++ src/components/Logos/LogoH.js
@@ -1,7 +1,7 @@
 import Link from "next/link";
 export default function LogoH({ className }) {
   return (
-    <Link href="/">
+    <Link href="/home">
       <img src="/horizontalLogo.png" width="150" alt="logo" />
     </Link>
   );
