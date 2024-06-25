8b7d1389f9b2b5336860d9df3a68425bb7091a23
geranhuns <gera.nuncio@gmail.com>
2024-06-24 21:16:30 -0600

    added responsiveness to bazares, login and register

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -3,7 +3,7 @@ export default function Login() {
   return (
     <main className=" flex justify-center items-center h-screen">
       <div className="bg-white p-10 rounded shadow-md flex flex-col items-center">
-        <h2 className="text-2xl mb-6">Iniciar sesión</h2>
+        <h2 className="text-xl lg:text-2xl mb-6">Iniciar sesión</h2>
         <LoginForm />
         <p className="mt-4">
           ¿No tienes una cuenta?{" "}
--- src/components/CreaTuBazarBanner/CreaTuBazarBanner.js
+++ src/components/CreaTuBazarBanner/CreaTuBazarBanner.js
@@ -2,7 +2,7 @@ import Button from "../Button/Button";
 export default function CreaTuBazarBanner() {
   return (
     <>
-      <div className="bg-patina-900 flex flex-col gap-10  text-raw-sienna-50 items-center rounded-md  py-8 ">
+      <div className="bg-patina-900 flex flex-col gap-10  text-raw-sienna-50 items-center rounded-md  py-8 w-72 md:w-80 lg:w-96 my-10">
         <h3 className="px-2 text-center">¿Quieres invitarnos a tu bazar?</h3>
         <Button
           text="Crea tu bazar"
