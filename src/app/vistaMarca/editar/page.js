9a9bd9842758968afc4ce543c709326d68892824
zambrix20 <companero20112011@hotmail.com>
2024-06-26 19:38:58 -0600

    feat: Add image preview functionality to FormMarca component
    The code changes include adding image preview functionality to the FormMarca component. Users can now select an image file for the brand logo, and a preview of the selected image will be displayed. This enhances the user experience by allowing them to see a visual representation of the logo before submitting the form.

--- src/app/vistaMarca/editar/page.js
+++ src/app/vistaMarca/editar/page.js
@@ -5,7 +5,6 @@ export default function EditarMarcaPage() {
     return (
         <div className="flex flex-col items-center ">
             <h1 className="text-3xl font-semibold text-center text-raw-sienna-500 p-5 ">Editar Marca</h1>
-            <img src="https://picsum.photos/500/500" alt="brand" width={300} height={300} className="rounded-full" />
             <FormMarca />
         </div>
     );
--- src/components/FormMarca/FormMarca.js
+++ src/components/FormMarca/FormMarca.js
@@ -1,8 +1,34 @@
-import React from 'react'
+"use client"
+import Image from 'next/image'
+import React, { useState } from 'react'
 
 function FormMarca() {
+
+    const [previewImagen, setPreviewImagen] = useState(null)
+
+    const handleImagen = (e) => {
+        const file = e.target.files[0]
+        if (file) {
+            const reader = new FileReader()
+            reader.onloadend = () => {
+                setPreviewImagen(reader.result)
+            }
+            reader.readAsDataURL(file)
+        }
+    }
+
     return (
         <form className="w-[50%] mx-auto mt-10">
+            <div className="flex flex-col gap-4">
+                <label htmlFor="logoMarca" className=' text-lg font-semibold text-raw-sienna-900'>Logo de la Marca</label>
+                <input type="file" id="logoMarca" className="border border-gray-300 p-2" onChange={handleImagen} />
+                <div className="flex justify-center">
+                    {previewImagen &&
+                        // <img src={previewImagen} alt="Logo de la Marca" className="w-20 h-20 object-cover" />
+                        <Image src={previewImagen} alt="Logo de la Marca" width={300} height={300} className="object-cover overflow-hidden border border-gray-300 rounded-custom1" />
+                    }
+                </div>
+            </div>
             <div className="flex flex-col gap-4">
                 <label htmlFor="nombreMarca">Nombre de la Marca</label>
                 <input type="text" id="nombreMarca" className="border border-gray-300 p-2" />
@@ -14,10 +40,6 @@ function FormMarca() {
             <div className="flex flex-col gap-4">
                 <label htmlFor="descripcionMarca">Descripción de la Marca</label>
                 <textarea id="descripcionMarca" className="border border-gray-300 p-2 w-full h-32 resize-none overflow-y-auto" />
-            </div>
-            <div className="flex flex-col gap-4">
-                <label htmlFor="logoMarca">Logo de la Marca</label>
-                <input type="file" id="logoMarca" className="border border-gray-300 p-2" />
             </div>
             <div className="flex flex-col gap-4">
                 <label htmlFor="redesSociales">Redes Sociales</label>
