90397d0c1c6b8990267449f37103852b9c61b7e1
Carlos Gastelum <carlosgastelum9923@gmail.com>
2024-07-03 13:53:22 -0600

    login funcionando, falta redireccion a sesion correspondiente

--- src/api/users/routes.js
+++ src/api/users/routes.js
@@ -1,26 +1,6 @@
 const MONGO_URL = 'http://localhost:3001/users';
 import Swal from 'sweetalert2'
 
-// export const registerUserFetch = async (data) => {
-//     try {
-//         const response = await fetch(`${MONGO_URL}/register`, {
-//           method: 'POST',
-//           headers: {
-//             'Content-Type': 'application/json'
-//           },
-//           body: JSON.stringify(data)
-//         });
-        
-//         if (response.ok) {
-//           console.log('Registro exitoso');
-          
-//         } else {
-//           console.error('Error al registrar');
-//         }
-//       } catch (error) {
-//         console.error('Error al realizar la solicitud:', error);
-//       } 
-// }
 
 export const registerUserFetch = async (data) => {
   try {
@@ -53,3 +33,49 @@ export const registerUserFetch = async (data) => {
       alert('Error al realizar la solicitud: ' + error.message);  // Mostrar error de solicitud
   }
 };
+
+
+export async function loginUserFetch(data) {
+  try {
+    const response = await fetch(`${MONGO_URL}/login`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    });
+    
+    if (response.ok) {
+      const data = await response.json();
+      // console.log('Mensaje del servidor:', data.msg);
+      // console.log('Token JWT recibido:', data.data);
+      // Guardar el token JWT en el localStorage
+      localStorage.setItem('jwtToken', data.data);
+ 
+      
+      return { success: true, data: data.data }; // Indica éxito y devuelve el token
+    } else {
+      const errorData = await response.json();
+      if (errorData.msg === "user not found") {
+        Swal.fire({
+        icon: "error",
+        title: "Usuario no registrado o incorrecto",
+        text: "No tienes cuenta?",
+        footer: '<a href="/register">Registrate aqui...</a>'
+      });
+      } else {
+        if(errorData.msg === "Incorrect password"){
+          Swal.fire({
+            icon: "error",
+            title: "Oops",
+            text: "Password incorrecto",
+          });
+        }
+      }
+      
+      
+    }
+  } catch (error) {
+    return { success: false, error: error }; // Indica error de red
+  }
+}
--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,10 +1,18 @@
+"use client";
 import LoginForm from "@/components/LoginForm/LoginForm";
+import { loginUserFetch } from "@/api/users/routes";
 export default function Login() {
+  
+  const dataLogin =((data)=>{
+    // console.log(data)
+    loginUserFetch(data)
+  })
+
   return (
     <div className=" flex justify-center items-center h-screen lg:max-w-screen-xl mx-auto">
       <div className="bg-white p-10 rounded shadow-md flex flex-col items-center">
         <h2 className="text-xl lg:text-2xl mb-6">Iniciar sesión</h2>
-        <LoginForm />
+        <LoginForm dataLoginProp={dataLogin} />
         <p className="mt-4">
           ¿No tienes una cuenta?{" "}
           <a href="/register" className="text-blue-500">
