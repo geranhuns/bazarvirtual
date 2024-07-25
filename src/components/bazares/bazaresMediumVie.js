0aee597a888bd6c29a6174e08aec009208c90c61
geranhuns <gera.nuncio@gmail.com>
2024-07-25 15:22:12 -0600

    fixed routes to get to bazar id

--- src/components/bazares/bazaresMediumVie.js
+++ src/components/bazares/bazaresMediumVie.js
@@ -1,9 +1,12 @@
+"use client";
 import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export default function BazarMediumView({ className, item, createdBy }) {
   const pathname = usePathname();
   const { profilePicture, time, date, username, _id } = item;
 
+  const [redirect, setRedirect] = useState();
   const obtenerFechaFormateada = (fechaCompleta) => {
     // Extrae la parte de la fecha 'yyyy-MM-dd' de la cadena 'yyyy-MM-ddTHH:mm:ss.sssZ'
     if (!fechaCompleta) return "";
@@ -14,9 +17,20 @@ export default function BazarMediumView({ className, item, createdBy }) {
 
     return `${dia}-${mes}`;
   };
+
+  const sendTo = () => {
+    if (createdBy) {
+      setRedirect(createdBy._id);
+    } else {
+      setRedirect(_id);
+    }
+  };
+  useEffect(() => {
+    sendTo();
+  }, []);
   return (
     <a
-      href={`bazares/${createdBy._id}`}
+      href={`bazares/${redirect}`}
       className={`flex flex-col bg-raw-sienna-200 py-4 rounded-lg gap-2 items-center cursor-pointer w-36 md:w-40 lg:w-48  ${className}`}
     >
       <div className="w-24 h-24 overflow-hidden rounded-full">
