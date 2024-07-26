65d6a3eef2c80fbd0f55511a4d7130e8b9ee5134
geranhuns <gera.nuncio@gmail.com>
2024-07-26 16:42:35 -0600

    suspenseboundary

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,30 +1,36 @@
+import { metadata, viewport } from "./metadata";
+
 import { Inter } from "next/font/google";
 import Footer from "@/components/Footer/Footer";
-import { metadata } from "./metadata";
 import Header from "@/components/Header/Header";
 import { HeaderProvider } from "@/components/HContext/HeaderContext"; //para acceder a contextos globales, en este caso acceder desde otra pagina a un state
 // import { CartProvider } from "@/components/CartContext/CartContext";
 import { UserProvider } from "@/components/UserContext/UserContext";
+import SuspenseBoundary from "@/components/SuspenseBoundary"; // Ajusta la ruta según tu estructura de carpetas
+import BusquedaProductos from "@/components/BusquedaProductos";
+
 import "@/app/globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export { metadata };
+export { metadata, viewport };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
       <body className="inter-class">
         <UserProvider>
-          {/* <CartProvider> */}
-          <HeaderProvider>
-            <Header />
-            <main className="flex flex-col h-full min-h-screen">
-              {children}
-            </main>
-            <Footer />
-          </HeaderProvider>
-          {/* </CartProvider> */}
+          <SuspenseBoundary>
+            {/* <CartProvider> */}
+            <HeaderProvider>
+              <Header />
+              <main className="flex flex-col h-full min-h-screen">
+                {children}
+              </main>
+              <Footer />
+            </HeaderProvider>
+            {/* </CartProvider> */}
+          </SuspenseBoundary>
         </UserProvider>
       </body>
     </html>
--- src/app/metadata.js
+++ src/app/metadata.js
@@ -1,27 +1,27 @@
 // metadata.js
 export const metadata = {
-    title: "Bazar Virtual Consume Local",
-    description: "Tienda virtual que apoya al comercio local",
-    authors: [
-      {
-        name: "Gerardo Nuncio",
-        name: "Carlos Gastélum",
-        name: "Arturo Zambrano",
-      },
-    ],
-    keywords: [
-      "tienda virtual",
-      "bazarvirtual.mx",
-      "comercio local",
-      "consume local",
-      "artesanías",
-      "artesanos locales",
-      "productos locales",
-      "bazares gdl",
-      "bazares mexicanos",
-    ],
-    publisher: "Bazar Virtual Consume Local",
-    referrer: "origin-when-crossorigin",
-    viewport: "width=device-width, initial-scale=1.0",
-  };
-  
+  title: "Bazar Virtual Consume Local",
+  description: "Tienda virtual que apoya al comercio local",
+  authors: [
+    {
+      name: "Gerardo Nuncio",
+      name: "Carlos Gastélum",
+      name: "Arturo Zambrano",
+    },
+  ],
+  keywords: [
+    "tienda virtual",
+    "bazarvirtual.mx",
+    "comercio local",
+    "consume local",
+    "artesanías",
+    "artesanos locales",
+    "productos locales",
+    "bazares gdl",
+    "bazares mexicanos",
+  ],
+  publisher: "Bazar Virtual Consume Local",
+  referrer: "origin-when-crossorigin",
+};
+
+export const viewport = "width=device-width, initial-scale=1.0";
