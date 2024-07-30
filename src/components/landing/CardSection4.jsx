8b7f04e63eb12e9358ea894e780c64d497575f4e
geranhuns <gera.nuncio@gmail.com>
2024-07-29 18:06:41 -0600

    fixed carousel bugs

--- src/components/landing/CardSection4.jsx
+++ src/components/landing/CardSection4.jsx
@@ -4,18 +4,18 @@ function CardSection4(props) {
   const { bgClass, iconCard, title, description1, description2, textButton, bgButton, hoverButton } = props;
   return (
     <>
-      <div className={`${bgClass}  w-1/2 h-full flex justify-center items-center max-sm:h-1/2 max-sm:w-full`}>
-        <div className="  flex flex-col items-center justify-around max-sm:justify-start  ">
-          <div className=" w-full h-2/6 flex flex-col items-center">
+      <div className={`${bgClass}  w-full lg:w-1/2 justify-center h-full flex py-10 `}>
+        <div className="  flex flex-col items-center justify-center px-12 ">
+          <div className=" w-full  flex flex-col items-center">
             {iconCard}
-            <h3 className="text-custom2 text-Eggshell text-center leading-custom4 font-bold max-sm:text-base">{title}</h3>
+            <h3 className="text-custom2 text-Eggshell text-center leading-custom4 font-bold  w-96">{title}</h3>
           </div>
-          <div className=" w-full h-3/6 text-2xl font-light text-center text-Eggshell flex flex-col justify-around items-center max-sm:text-sm">
-            <ul className="list-disc list-inside">
+          <div className=" w-full h-3/6 text-2xl font-light text-center text-Eggshell flex flex-col justify-around items-center ">
+            <ul className="list-disc list-inside h-72 flex flex-col justify-center">
               <li >{description1}</li>
               <li>{description2}</li>
             </ul>
-            <button className={`w-2/3 h-11 ${bgButton} text-xl rounded-2  xl font-semibold max-sm:text-sm max-sm:h-8 max-sm:w-10/12 max-sm:mt-1 ${hoverButton}`}>
+            <button className={`w-2/3 h-11 ${bgButton} text-xl flex justify-center items-center rounded-sm font-semibold  py-3 ${hoverButton}`}>
               <a href="/register">
                 {textButton}
               </a>
