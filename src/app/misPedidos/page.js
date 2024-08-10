bc81cc1a349ec456f14cb15a02f01998315c0a06
geranhuns <gera.nuncio@gmail.com>
2024-08-09 19:10:26 -0600

    misPedidos from client is now working

--- src/app/misPedidos/page.js
+++ src/app/misPedidos/page.js
@@ -1,15 +1,17 @@
 "use client";
 import Pedido from "@/components/pedidosActivos/Pedido";
+import PedidoGrupo from "@/components/pedidosActivos/PedidoGrupo";
 import Dropdown from "@/components/Dropdown/Dropdown";
 
 import { useEffect, useState } from "react";
 import { useUserContext } from "@/components/UserContext/UserContext";
-import { fetchPurchaseHistory } from "@/api/users/productLists/routes";
+import { fetchClientPurchases } from "@/api/orders/routes";
 
 export default function PedidosActivos() {
   //falta hacer el estado del select y toda la logica correspondiente para mostrar lo que esté seleccionado.
-  const [searchCategory, setSearchCategory] = useState("Todo");
+  const [searchCategory, setSearchCategory] = useState("Todos los pedidos");
   const [purchaseHistory, setPurchaseHistory] = useState([]);
+  const [displayedProducts, setDisplayedProducts] = useState([]);
 
   const { user } = useUserContext();
 
@@ -29,28 +31,39 @@ export default function PedidosActivos() {
     }
 
     try {
-      const productsHistory = await fetchPurchaseHistory(user.id);
+      const productsHistory = await fetchClientPurchases(user.id);
 
-      // Inicializar el array para agrupar los productos por compra
-      const allBoughtProducts = productsHistory.map((item) => {
-        const { purchaseId, items } = item;
-        // Crear un objeto con el purchaseId y los items de esa compra
-        return {
-          purchaseId,
-          items,
-        };
-      });
-
-      setPurchaseHistory(allBoughtProducts);
-      console.log(allBoughtProducts); // Imprime la lista de compras agrupadas
+      setPurchaseHistory(productsHistory.data);
     } catch (error) {
       console.error("Error fetching product history:", error);
     }
   };
 
   useEffect(() => {
+    if (searchCategory !== "Todos los pedidos") {
+      setDisplayedProducts(
+        purchaseHistory.filter((producto) => producto.pendingDelivery)
+      );
+    } else {
+      setDisplayedProducts(purchaseHistory);
+    }
+  }, [searchCategory, purchaseHistory]);
+
+  useEffect(() => {
     if (user && user.id) getProductHistory();
   }, [user]);
+
+  const handleProductDelivered = (deliveredPurchaseId, deliveredProductId) => {
+    setPurchaseHistory((prevProducts) =>
+      prevProducts.filter(
+        (product) =>
+          !(
+            product.purchaseId === deliveredPurchaseId &&
+            product.productId._id === deliveredProductId
+          )
+      )
+    );
+  };
 
   return (
     <div className="flex flex-col  items-center lg:max-w-screen-xl mx-auto overflow-auto ">
@@ -76,31 +89,45 @@ export default function PedidosActivos() {
         )}
         {user.role !== "marca" && (
           <>
-            <Dropdown
-              options={optionsCliente}
-              className={"text-lg w-56"}
-              setSearchCategory={setSearchCategory}
-            />
+            <select
+              id="changeDisplayedProducts"
+              value={searchCategory} // Asegúrate de que 'quantity' sea un valor primitivo
+              onChange={(e) => {
+                setSearchCategory(e.target.value); // Pasar el valor como string
+              }}
+              className={`h-8 text-lg w-56 pl-1  bg-raw-sienna-200 text-raw-sienna-900 `}
+            >
+              {user.role === "cliente" &&
+                optionsCliente.map((option, index) => (
+                  <option value={option} key={index}>
+                    {option}
+                  </option>
+                ))}
+              {user.role === "marca" &&
+                optionsCliente.map((option, index) => (
+                  <option value={option} key={index}>
+                    {option}
+                  </option>
+                ))}
+            </select>
             <p className="pb-8 ">
               Consulta la página de detalle del pedido para contactar a la marca
               y ponerse de acuerdo para la entrega. Una vez recibido el
               producto, da click en el botón Recibido.
             </p>
             <hr className="h-0.5 bg-raw-sienna-400" />
-
-            {purchaseHistory.map((item) => {
-              console.log(item);
-
-              return (
-                <>
-                  <Pedido key={item.purchaseId} productGroup={item.items} />
-                  <hr className="h-0.5 bg-raw-sienna-400" />
-                </>
-              );
-            })}
-            {/* <PedidoCliente showButton={true} />
-            <PedidoCliente showButton={true} />
-            <PedidoCliente showButton={true} /> */}
+            <div>
+              {displayedProducts.map((item) => {
+                console.log(item._id);
+                return (
+                  <PedidoGrupo
+                    key={item._id}
+                    producto={item}
+                    onProductDelivered={handleProductDelivered}
+                  />
+                );
+              })}
+            </div>
           </>
         )}
       </div>
--- src/components/pedidosActivos/PedidoGrupo.js
+++ src/components/pedidosActivos/PedidoGrupo.js
@@ -1,50 +1,62 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Button from "../Button/Button";
-export default function PedidoGrupo({ showButton, singleProduct, quantity }) {
-  //Onclick del button se debe actualizar en la base de datos el deliveryPending del item seleccionado
+import { markProductAsDelivered } from "@/api/orders/routes";
+
+export default function PedidoGrupo({ producto, onProductDelivered }) {
+  const [pendingDelivery, setPendingDelivery] = useState(
+    producto.pendingDelivery
+  );
+
+  const markProductAsReceived = async () => {
+    await markProductAsDelivered(producto.purchaseId, producto.productId._id);
+    setPendingDelivery(false);
+    onProductDelivered(producto.purchaseId, producto.productId._id);
+  };
+
   return (
-    <>
-      <div className="flex flex-row  bg-raw-sienna-50 md:py-5 px-4 rounded-md">
-        <img
-          className="rounded-md self-center"
-          src={singleProduct.productImage}
-          width="100px"
-          heigth="100px"
-          alt="producto"
-        />
-        <div className="flex w-full flex-col md:flex-row  my-2 md:mt-0 items-center ">
-          <div className="pl-10 flex flex-col md:flex-row md:justify-between w-full md:gap-4">
-            <div className="hidden md:flex flex-col">
-              <h3 className="  text-base italic  ">Nombre de la Marca</h3>
-              <h3 className="  text-lg  ">
-                {singleProduct.createdBy.username}
-              </h3>
-            </div>
-
-            <div>
-              <h3 className="  text-base italic">Producto</h3>
-              <h3 className="  md:text-lg  ">{singleProduct.title}</h3>
-            </div>
-
-            <div className="flex md:flex-col gap-2 items-center ">
-              <h4 className="  text-base italic">Cantidad </h4>
-              <h4 className="bg-raw-sienna-200 w-9 text-center text-raw-sienna-900 rounded-sm py-1">
-                {quantity}
-              </h4>
-            </div>
+    <div className="flex flex-row bg-raw-sienna-50 md:py-5 px-4 rounded-md">
+      <img
+        className="rounded-md self-center"
+        src={producto.productId.productImage}
+        width="100px"
+        height="100px"
+        alt="producto"
+      />
+      <div className="flex w-full flex-col md:flex-row my-2 md:mt-0 items-center">
+        <div className="pl-10 flex flex-col md:flex-row md:justify-between w-full md:gap-4">
+          <div className="hidden md:flex flex-col">
+            <h3 className="text-base italic">Nombre de la Marca</h3>
+            <h3 className="text-lg">{producto.brandId.username}</h3>
           </div>
-          {showButton && (
-            <div className="flex flex-col pl-10 md:items-start md:mt-0 items-center md:pt-0 mt-2 ">
-              <Button
-                text={"Recibido"}
-                type={"button"}
-                variant={"yellow"}
-                className={"px-6"}
-              />
-            </div>
-          )}
+          <div>
+            <h3 className="text-base italic">Producto</h3>
+            <h3 className="md:text-lg">{producto.productId.title}</h3>
+          </div>
+          <div className="flex md:flex-col gap-2 items-center">
+            <h4 className="text-base italic">Cantidad</h4>
+            <h4 className="bg-raw-sienna-200 w-9 text-center text-raw-sienna-900 rounded-sm py-1">
+              {producto.quantity}
+            </h4>
+          </div>
         </div>
+        {pendingDelivery && (
+          <div className="flex flex-col pl-10 md:items-start md:mt-0 items-center md:pt-0 mt-2">
+            <Button
+              text={"Recibido"}
+              type={"button"}
+              variant={"yellow"}
+              className={"px-6"}
+              onClick={markProductAsReceived}
+            />
+          </div>
+        )}
+        {!pendingDelivery && (
+          <div className="flex flex-col pl-10 md:items-start md:mt-0 items-center md:pt-0 mt-2">
+            <h3 className="text-center">¡Gracias por tu compra!</h3>
+          </div>
+        )}
       </div>
-    </>
+      <hr className="h-0.5 bg-raw-sienna-400" />
+    </div>
   );
 }
