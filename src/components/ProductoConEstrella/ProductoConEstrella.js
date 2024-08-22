67c9e287b4bd2a54e803a01ed819baa249fc5ef4
geranhuns <gera.nuncio@gmail.com>
2024-08-22 11:29:22 -0600

    modified buttons width

--- src/components/ProductoConEstrella/ProductoConEstrella.js
+++ src/components/ProductoConEstrella/ProductoConEstrella.js
@@ -17,11 +17,11 @@ const ProductoConEstrella = ({ imageUrl, altText, addToWishList }) => {
   };
 
   return (
-    <div className="relative inline-block w-80">
+    <div className="relative inline-block w-full">
       <img src={imageUrl} alt={altText} className="w-full h-auto rounded-md" />
       <div className="cursor-pointer" onClick={handleClick}>
         {activeStar === false ? (
-          <FaRegStar className="absolute md:bottom-6 md:left-6 bottom-2 left-2 text-xl md:text-4xl" />
+          <FaRegStar className="absolute md:bottom-6 md:left-6 bottom-2 left-2 text-xl md:text-4xl text-yellow-bazar" />
         ) : (
           <FaStar className="absolute md:bottom-6 md:left-6 bottom-2 left-2 text-yellow-bazar text-xl md:text-4xl" />
         )}
