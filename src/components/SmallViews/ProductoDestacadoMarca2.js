8c06b28cca66df4a09f7a150f8b392b4ff3d8c79
geranhuns <gera.nuncio@gmail.com>
2024-08-29 18:45:46 -0600

    added ui to productodestacadomarca2

--- src/components/SmallViews/ProductoDestacadoMarca2.js
+++ src/components/SmallViews/ProductoDestacadoMarca2.js
@@ -6,17 +6,20 @@ export default function ProductoDestacadoMarca2({
   profile,
   nameMarca,
   imageProductos,
-})
-
-{
-  
+}) {
   return (
-    <div className={`flex flex-col  self-start bg-raw-sienna-50  p-1  rounded-md    h-[60vh] md:h-96  md:w-80 mx-auto   `}>
-      <div className="h-full w-full">
-        <MarcaSmallView2 className={"h-3/12 flex-col "} profilePicture={profile}  brand={nameMarca}/>
-      
+    <div
+      className={`flex flex-col  self-start bg-raw-sienna-50    rounded-md    h-[60vh] md:h-96  md:w-80 mx-auto py-8`}
+    >
+      <div className="h-full w-full ">
+        <MarcaSmallView2
+          className={"h-3/12 flex-col "}
+          profilePicture={profile}
+          brand={nameMarca}
+        />
+
         <div
-          className={`h-9/12 w-full grid gap-1 ${
+          className={`h-9/12 w-full grid  ${
             imageProductos.length === 1
               ? "grid-cols-1"
               : imageProductos.length === 2
@@ -24,8 +27,10 @@ export default function ProductoDestacadoMarca2({
               : imageProductos.length === 3
               ? "grid-cols-2 grid-rows-2"
               : "grid-cols-2 grid-rows-2"
-          } ${imageProductos.length === 4 ? "grid-rows-2" : ""}`}>
-            {imageProductos && imageProductos.slice(0, 4).map((product, index) => {
+          } ${imageProductos.length === 4 ? "grid-rows-2" : ""}`}
+        >
+          {imageProductos &&
+            imageProductos.slice(0, 4).map((product, index) => {
               return (
                 <div
                   className={`${
@@ -39,13 +44,12 @@ export default function ProductoDestacadoMarca2({
                         : "col-span-1 row-span-1"
                       : ""
                   }`}
-                  key={product.id}>
+                  key={product.id}
+                >
                   <img
-                    className={`w-full p-1 ${
-                      imageProductos.length === 1
-                        ? "h-60"
-                        : "h-[20vh] md:h-32"
-                    } object-cover overflow-hidden rounded-lg`}
+                    className={`w-full  ${
+                      imageProductos.length === 1 ? "h-60" : "h-[20vh] md:h-32"
+                    } object-cover overflow-hidden `}
                     src={product.image}
                     alt={`product-${product.id}`}
                   />
@@ -53,8 +57,6 @@ export default function ProductoDestacadoMarca2({
               );
             })}
         </div>
-
-
       </div>
     </div>
   );
